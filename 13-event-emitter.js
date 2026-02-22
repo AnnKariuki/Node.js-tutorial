acd7bbb2418f9dff6fe3eba63c7a4c6c8df463da
Ann Kariuki <awk19@duke.edu>
2026-02-21 21:55:20 -0800

    events emitter

---
+++ 13-event-emitter.js
@@ -0,0 +1,23 @@
+// get back the class
+// if want custom extend from class
+// otherwise just for emitting and handling events create instance
+const EventEmitter = require('events');
+
+// object/instance of the class
+const customEmitter = new EventEmitter();
+
+
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
+//event name, listener - callback function
+customEmitter.on('response', (name, id)=> {
+  console.log(`data received user ${name} with id:${id}`)
+})
+
+customEmitter.on('response', ()=> {
+  console.log(`some other logic here`)
+})
+
+customEmitter.emit('response', 'john', 34);
